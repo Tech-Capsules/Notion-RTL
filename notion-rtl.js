aaee4b0295eee64ce980647031c6c2efe532a809
Ammar Yaser <ammaryaser.dev@gmail.com>
2024-05-13 20:29:14 +0300

    Add RTL support for Notion app

---
+++ notion-rtl.js
@@ -0,0 +1,27 @@
+const targetNode = document.body;
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    const newNodes = [...mutation.addedNodes].filter(
+      (n) => n.nodeType === Node.TEXT_NODE
+    );
+
+    if (newNodes.length > 0) {
+      for (let i = 0; i < newNodes.length; i++) {
+        const node = newNodes[i];
+
+        const textContent = node.textContent;
+        const arabic = /[\u0600-\u06FF]/;
+
+        if (textContent && arabic.test(textContent)) {
+          node.parentNode.setAttribute("dir", "rtl");
+        }
+      }
+    }
+  });
+});
+
+observer.observe(targetNode, {
+  childList: true,
+  subtree: true,
+});
